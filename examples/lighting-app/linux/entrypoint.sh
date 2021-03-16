3f4ab459cfb0f46ac2c92cf6d4b2ac4fc2d5eeb4
Song Guo <songguo@google.com>
2021-03-16 16:23:13 +0800

    Add dbus-monitor

--- examples/lighting-app/linux/entrypoint.sh
+++ examples/lighting-app/linux/entrypoint.sh
@@ -3,6 +3,7 @@
 service dbus start
 sleep 1
 service avahi-daemon start
+dbus-monitor --system --profile &
 /usr/sbin/otbr-agent -I wpan0 spinel+hdlc+uart:///dev/ttyUSB0 &
 sleep 1
 ot-ctl panid 0x1234
