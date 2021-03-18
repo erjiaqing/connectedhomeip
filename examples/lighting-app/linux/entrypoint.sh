9733906a04b039d69e89896e8b02f61a721e8b99
Song Guo <songguo@google.com>
2021-03-18 16:40:39 +0800

    Add more logs

--- examples/lighting-app/linux/entrypoint.sh
+++ examples/lighting-app/linux/entrypoint.sh
@@ -4,7 +4,7 @@ service dbus start
 sleep 1
 service avahi-daemon start
 dbus-monitor --system --profile &
-/usr/sbin/otbr-agent -I wpan0 spinel+hdlc+uart:///dev/ttyUSB0 &
+/usr/sbin/otbr-agent -d7 -v -I wpan0 spinel+hdlc+uart:///dev/ttyUSB0 &
 sleep 1
 ot-ctl panid 0x1234
 ot-ctl ifconfig up
